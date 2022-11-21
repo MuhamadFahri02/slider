98dec0a338cb8b0bc58de26ce27b2d5eef5d8b40
MuhamadFahri02 <pariraps09@gmail.com>
2022-11-22 02:46:48 +0700

    slider

---
+++ src/images.jsx
@@ -0,0 +1,7 @@
+import image1 from "./assets/Group1.png";
+import image2 from "./assets/Group2.png";
+import image3 from "./assets/Group3.png";
+import image4 from "./assets/Group4.png";
+import image5 from "./assets/Group5.png";
+
+export default [image1, image2, image3, image4, image5];
